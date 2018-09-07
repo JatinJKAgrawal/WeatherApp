2d1ef1773d10b4790a759eff4f70f07187666a7e
jatin-sofocle <jatin.agrawal@sofocle.com>
2018-09-07 11:31:56 +0530

    address input configuration.

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-var request = require('request');
+const request = require('request');
+const yargs = require('yargs');
+const argv = yargs
+.options({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address we need the weather information for.',
+    string: true
+  }
+})
+.help()
+.alias('help','h')
+.argv;
+
+const address = argv.a;
+
+
 request({
-  url: 'http://www.mapquestapi.com/geocoding/v1/address?key=bVtMG1JExT6A2A5ibZzA4RArrsHU0Pfy&location=A%2038%20sector%202%20Noida',
+  url: `http://www.mapquestapi.com/geocoding/v1/address?key=bVtMG1JExT6A2A5ibZzA4RArrsHU0Pfy&location=${encodeURIComponent(address)}`,
   json: true
 }, (error, response, body) => {
   console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}\nLongitude: ${body.results[0].locations[0].latLng.lng}`);
