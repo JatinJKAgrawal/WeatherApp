0dbec5f39a41b480d5df66fcea58a8966f9031f8
jatin-sofocle <jatin.agrawal@sofocle.com>
2018-09-07 12:32:07 +0530

    Error Handling!

--- app.js
+++ app.js
@@ -20,5 +20,11 @@ request({
   url: `http://www.mapquestapi.com/geocoding/v1/address?key=bVtMG1JExT6A2A5ibZzA4RArrsHU0Pfy&location=${encodeURIComponent(address)}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}\nLongitude: ${body.results[0].locations[0].latLng.lng}`);
+  if(error){
+    console.log('unable to connect to api servers..');
+  }else if(!body.results[0].locations[0]){
+    console.log('unable to retrieve address..\n\nTry entering valid address!\n');
+  }else{
+      console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}\nLongitude: ${body.results[0].locations[0].latLng.lng}`);
+  }
 });
