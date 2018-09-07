5721f5594a9d012f69c9481abf799772a34c0bb1
jatin-sofocle <jatin.agrawal@sofocle.com>
2018-09-07 14:58:12 +0530

    Refactoring

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/latLng.js');
 const argv = yargs
 .options({
   a: {
@@ -13,18 +13,6 @@ const argv = yargs
 .alias('help','h')
 .argv;
 
-const address = argv.a;
-
-
-request({
-  url: `http://www.mapquestapi.com/geocoding/v1/address?key=bVtMG1JExT6A2A5ibZzA4RArrsHU0Pfy&location=${encodeURIComponent(address)}`,
-  json: true
-}, (error, response, body) => {
-  if(error){
-    console.log('unable to connect to api servers..');
-  }else if(!body.results[0].locations[0]){
-    console.log('unable to retrieve address..\n\nTry entering valid address!\n');
-  }else{
-      console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}\nLongitude: ${body.results[0].locations[0].latLng.lng}`);
-  }
-});
+var latLng = geocode.getLatLng(argv.a, (latLng)=>{
+   console.log(`Latitude: ${latLng[0]}\nLongitude: ${latLng[1]}`);
+});//returns [Latitude, Longitude] & handles errors
---
+++ geocode/latLng.js
@@ -0,0 +1,20 @@
+var request = require('request');
+
+var getLatLng = (address, callback) => {
+  request({
+    url: `http://www.mapquestapi.com/geocoding/v1/address?key=bVtMG1JExT6A2A5ibZzA4RArrsHU0Pfy&location=${encodeURIComponent(address)}`,
+    json: true
+  }, (error, response, body) => {
+    if(error){
+      console.log('unable to connect to api servers..');
+    }else if(!body.results[0].locations[0]){
+      console.log('unable to retrieve address..\n\nTry entering valid address!\n');
+    }else{
+      return  callback([body.results[0].locations[0].latLng.lat, body.results[0].locations[0].latLng.lng]);
+    }
+  });
+}
+
+module.exports = {
+  getLatLng
+}
